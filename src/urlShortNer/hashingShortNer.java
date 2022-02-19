b7ac5d3d87675ae4c7bbbf0d1516daace47a286a
Pratik Das <pratik14901@gmail.com>
2022-02-20 00:09:39 +0530

    function to generate random string

--- src/urlShortNer/hashingShortNer.java
+++ src/urlShortNer/hashingShortNer.java
@@ -5,11 +5,20 @@ import java.util.Scanner;
 
 public class hashingShortNer {
 
-//    static String shortString(int n) {
-//        String collection = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
-//
-//        StringBuilder str = new StringBuilder()
-//    }
+    static String shortString(int n) {
+        String collection = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+
+        StringBuilder str = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+            int index = (int)(collection.length()*Math.random());
+            str.append(collection.charAt(i));
+        }
+
+        return str.toString();
+    }
+
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("URL ShortNer");
