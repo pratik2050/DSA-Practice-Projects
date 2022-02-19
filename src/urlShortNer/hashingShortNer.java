fdb6ea6d96841ed32dcd12e77b1640217c915abe
Pratik Das <pratik14901@gmail.com>
2022-02-20 00:06:51 +0530

    started hashing

--- src/urlShortNer/hashingShortNer.java
+++ src/urlShortNer/hashingShortNer.java
@@ -1,10 +1,38 @@
 package urlShortNer;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class hashingShortNer {
+
+//    static String shortString(int n) {
+//        String collection = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+//
+//        StringBuilder str = new StringBuilder()
+//    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Your Large URL: ");
+        System.out.println("URL ShortNer");
+        int choice;
+        while (choice != 0) {
+            System.out.println("What You want to do?\nType 1 For Encode and 2 For Decode (OR) Type 0 to Exit: ");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            HashMap<String, String> url = new HashMap<>();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Url: ");
+                    String str = sc.next();
+                    int pos = 0;
+                    for (int i = 0; i < str.length(); i++) {
+                        if (str.charAt(i) == '/') {
+                            pos = i+1;
+                            break;
+                        }
+                    }
+
+            }
+        }
     }
 }
