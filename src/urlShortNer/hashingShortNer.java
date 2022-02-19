ebac1b598dea4b1dad088034270c76f69cf291ca
Pratik Das <pratik14901@gmail.com>
2022-02-20 00:41:44 +0530

    almost done

--- src/urlShortNer/hashingShortNer.java
+++ src/urlShortNer/hashingShortNer.java
@@ -1,6 +1,7 @@
 package urlShortNer;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class hashingShortNer {
@@ -11,37 +12,60 @@ public class hashingShortNer {
         StringBuilder str = new StringBuilder(n);
 
         for (int i = 0; i < n; i++) {
-            int index = (int)(collection.length()*Math.random());
-            str.append(collection.charAt(i));
+            int index = (int) (collection.length() * Math.random());
+            str.append(collection.charAt(index));
         }
 
         return str.toString();
     }
 
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("URL ShortNer");
-        int choice;
-        while (choice != 0) {
-            System.out.println("What You want to do?\nType 1 For Encode and 2 For Decode (OR) Type 0 to Exit: ");
-            choice = sc.nextInt();
-            sc.nextLine();
 
-            HashMap<String, String> url = new HashMap<>();
-            switch (choice) {
-                case 1:
-                    System.out.println("Enter Url: ");
-                    String str = sc.next();
-                    int pos = 0;
-                    for (int i = 0; i < str.length(); i++) {
-                        if (str.charAt(i) == '/') {
-                            pos = i+1;
-                            break;
-                        }
-                    }
+        int choice = Integer.MAX_VALUE;
+
+        System.out.println("What You want to do?\nType 1 For Encode and 2 For Decode (OR) Type 0 to Exit: ");
+        choice = sc.nextInt();
+        sc.nextLine();
+
+        int pos = 0;
+
+        HashMap<String, String> url = new HashMap<>();
+
+
+        System.out.println("Enter Url: ");
+        String str = sc.next();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '/') {
+                pos = i + 1;
 
             }
         }
+        String key = shortString(5);
+
+        StringBuilder shortStr = new StringBuilder();
+        for (int i = 0; i < pos; i++) {
+            shortStr.append(str.charAt(i));
+        }
+
+        System.out.println("New shorthand url: " + shortStr + key);
+
+        url.put(key, str.substring(pos));
+
+
+        StringBuilder outString = new StringBuilder();
+        for (int i = 0; i < pos; i++) {
+            outString.append(str.charAt(i));
+        }
+        for (Map.Entry<String, String> e : url.entrySet()) {
+            if (e.getKey() == key) {
+                outString.append(e.getValue());
+
+            }
+        }
+        System.out.println(outString);
+
     }
 }
