724b9baa14ed35ec144823c45ef4363864aad1ce
Pratik Das <pratik14901@gmail.com>
2022-02-19 23:43:45 +0530

    initial commit

---
+++ src/urlShortNer/hashingShortNer.java
@@ -0,0 +1,10 @@
+package urlShortNer;
+
+import java.util.Scanner;
+
+public class hashingShortNer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Large URL: ");
+    }
+}
